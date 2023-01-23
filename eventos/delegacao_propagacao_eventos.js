94a95ee8bc8ea5e9a5cd065678b5297151621115
Pichau <gustavosandrindattein123@gmail.com>
2023-01-23 20:42:32 -0300

    delegação e propagação de eventos. Método on()

---
+++ eventos/delegacao_propagacao_eventos.js
@@ -0,0 +1,15 @@
+$('#books_list tbody').on('click', 'tr', function(e){
+    $(e.delegateTarget).find('tr').removeClass('trselected');
+    $(this).addClass('trselected');
+    
+})
+$('#btn_add_book').on('click', function(){
+        let tr = `
+            <tr>
+                <td>Til</td>
+                <td>José de Alencar</td>
+            </tr>
+        `;
+
+        $('#books_list tbody').append(tr);
+    });
---
+++ eventos/exemplo_metodo_on.js
@@ -0,0 +1,16 @@
+ // Exemplo método on()
+/*
+$('#books_list tbody td').on('click contextmenu', function(e){
+    if (e.type == 'contextmenu') e.preventDefault();
+
+    $('#books_list tbody td').css({
+        backgroundColor: 'initial',
+        color: 'initial'
+    })
+
+    $(this).css({
+        backgroundColor: 'black',
+        color: 'white',
+    })
+})
+*/
